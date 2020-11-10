33627dcc69f23514ff437050228a863d747a4909
Darica Louie <darica.louie@gmail.com>
2020-11-10 02:27:19 -0800

    background image, lots of styling tweaks

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ var loginAttempts = 0;
 const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
-const forgottenpassword = document.getElementById("forgot-password");
+const forgottenPassword = document.getElementById("forgot-password");
 
 const searchbar = document.getElementById("search-bar");
 const searchButton = document.getElementById("search-submit");
 
+const resultsWindow = document.getElementById("results-window");
 const results = document.getElementById("search-results");
 
 loginButton.addEventListener("click", (e) => {
@@ -24,22 +25,28 @@ loginButton.addEventListener("click", (e) => {
         loginErrorMsg.style.opacity = 1;
         loginAttempts++;
         if(loginAttempts > 2){
-            forgottenpassword.style.opacity = 1;
+            forgottenPassword.style.opacity = 1;
         }
     }
 })
 
-searchButton.addEventListener("click", (e) => {
-    console.log("search button pressed");
+function searchHandler(){
     searchterm = searchbar.search.value;
     results.innerHTML = "No results found for \"" + searchterm + "\". Please refine your search.";
+    resultsWindow.style.maxHeight = resultsWindow.scrollHeight +"px";
+    resultsWindow.style.padding = "20px 18px" ;    
+    
+}
+
+searchButton.addEventListener("click", (e) => {
+    console.log("search button pressed");
+    searchHandler();
 })
 searchbar.addEventListener('submit', event => {
     // submit event detected
     event.preventDefault()
     console.log("search submitted");
-    searchterm = searchbar.search.value;
-    results.innerHTML = "No results found for \"" + searchterm + "\". Please refine your search.";
+    searchHandler();
   })
 
 function checkLoggedIn (){
